58f7ca388c326ad301c99325aee413dc25b16f0b
zhanghaidi <884776490@qq.com>
2023-03-16 09:59:21 +0800

    新建springboot项目

---
+++ spring-boot-basic/src/test/java/com/basic/springbootbasic/SpringBootBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basic.springbootbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
